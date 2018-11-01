5118cc9a35376965314f200e29bef3ed73c06078
wizarDDog <k1993g@gmail.com>
2018-11-01 15:22:42 +0200

    create react-app

---
+++ notes-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>This is Notes app</div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
