462189375a8f01fbdacbad521e98e5d9ab175505
wizarDDog <k1993g@gmail.com>
2018-11-04 15:55:03 +0200

    show list of notes

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,29 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faPlus, faTrash} from '@fortawesome/free-solid-svg-icons'
+
 library.add(faPlus, faTrash)
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App grid-container">
-        <header className="App-header">
-            <div className="Name grid-item">Name</div>
-            <div className="grid-item">
-                <div id="Icon" ><FontAwesomeIcon icon="plus" /></div>
-                <div id="Icon" ><FontAwesomeIcon icon="trash" /></div>
-                <div id="Icon" ><FontAwesomeIcon icon="ghost" /></div>
+    constructor(props){
+        super(props);
+        this.state = {
+            note: "",
+            savedNote: "",
+            writtenNotes: []
+        }
+    }
+
+    saveNote(e){
+        this.state.note = e.target.value;
+
+    }
+
+    showSavedNote() {
+        var newNote = this.state.note
+        this.setState({
+            savedNote : newNote,
+        })
+        this.state.writtenNotes.push(newNote)
+        document.getElementById('input').value = ''
+    }
+
+
+    render() {
+        return (
+            <div className="App ">
+                <div className="Name ">Name</div>
+                <button type="submit" onClick={()=> this.showSavedNote()} id="Icon1"><FontAwesomeIcon icon="plus"/></button>
+                <button disabled id="Icon2"><FontAwesomeIcon icon="trash"/></button>
+                <ul className="WrittenNotes">
+                    {this.state.writtenNotes.map((item ,i) => {
+                        return <li key={i} >{item}</li>
+                    })}</ul>
+                <input className="Note" id="input" placeholder="Write your note here!" cols={40} rows={10}
+                          onChange={(e) => this.saveNote(e)} defaultValue={this.state.note}/>
             </div>
-        </header>
-          <div className="Note grid-item"><textarea cols={40} rows={10}/></div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
