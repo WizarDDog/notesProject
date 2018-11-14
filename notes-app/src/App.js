3fc52a525c71fb057f39e1da0c32951fc145356d
wizarDDog <k1993g@gmail.com>
2018-11-14 21:24:54 +0200

    +full styling +full functionality

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
             isEnable: false,
             showMenu: false,
             colorPick: "",
+            showNoteD: false,
 
             buttonNewNote: false,
         };
@@ -44,6 +45,9 @@ class App extends Component {
             this.findNoteOfName(i);
             this.setState({
                 isHidden: false,
+
+                showNoteD: true
+
             })
         }
         if (!this.state.isHidden) {
@@ -52,7 +56,8 @@ class App extends Component {
                 isHidden: true,
                 showNote: "",
                 showEdit: false,
-                isBold: false
+                isBold: false,
+
             })
         }
     }
@@ -89,8 +94,8 @@ class App extends Component {
 
 
 
-        document.getElementById('input2').value = '';
-        document.getElementById('input1').value = '';
+        document.getElementById('text').value = '';
+        document.getElementById('title').value = '';
         this.setState({
             note: "",
             notesName: "",
@@ -115,7 +120,8 @@ class App extends Component {
             this.allNotes.splice(index, 1);
             this.color.splice(index, 1);
             this.setState({
-                showNote: ""
+                showNote: "",
+                isHidden: true,
             })
         }
     }
@@ -128,7 +134,8 @@ class App extends Component {
             this.setState({
                 note: this.state.showNote,
                 notesName: oldNotesName,
-                showEdit: true
+                showEdit: true,
+                showNoteD: false,
             });
             this.showEditNote()
         }
@@ -136,12 +143,16 @@ class App extends Component {
 
     showEditNote() {
         if (this.state.showEdit) {
-            return <div>
-                <input defaultValue={this.state.notesName} onChange={(e) => this.state.newNotesName = e.target.value}
-                       placeholder={this.state.notesName}/>
-                <textarea rows="4" cols="50"
-                          onChange={(e) => this.state.newNote = e.target.value}>{this.state.showNote}</textarea>
-                <button onClick={() => this.saveEditNote()}>Save</button>
+            return <div id="wrapper">
+                <form id="paper" method="get" action="">
+                    <div id="margin">Notes Name: <input id="title" type="text" name="title" placeholder="Notes name!"
+                                                        onChange={(e) => this.state.newNotesName = e.target.value} defaultValue={this.state.notesName}/></div>
+                    <textarea onChange={(e) => this.state.newNote = e.target.value}   id="text" name="text" rows="4"
+                    >{this.state.showNote}</textarea>
+                </form>
+                <button id="button"  onClick={() => this.saveEditNote()}>
+                    Save
+                </button>
             </div>
         }
         if (!this.state.showEdit) {
@@ -198,13 +209,20 @@ class App extends Component {
             colorPick: "red"
         })
     }
+    makeNone(){
+        this.setState({
+            showMenu: false,
+            colorPick: null
+        })
+    }
 
     showColorPick(){
         if (this.state.showMenu){
            return <div>
-                <button className="green" onClick={()=> this.makeGreen()}>Green</button>
-                <button className="blue" onClick={()=> this.makeBlue()}>Blue</button>
-                <button className="red" onClick={()=> this.makeRed()}>Red</button>
+                <button className="greenButton" onClick={()=> this.makeGreen()}>Green</button>
+                <button className="blueButton" onClick={()=> this.makeBlue()}>Blue</button>
+                <button className="redButton" onClick={()=> this.makeRed()}>Red</button>
+                <button className="none" onClick={()=> this.makeNone()}>None</button>
             </div>
         }
     }
@@ -217,59 +235,65 @@ class App extends Component {
 
     newNote() {
         if (this.state.buttonNewNote) {
-            return <div>
-                <div className="form-group">
+            return <div id="wrapper">
 
-                    <input className="notesName"  id="input1" placeholder="Notes name!"
-                           onChange={(e) => this.saveNotesName(e)} defaultValue={this.state.notesName}/>
-                    <div id="label" >  Notes name!</div>
+                <form id="paper" method="get" action="">
 
-                        <button className={this.state.colorPick}  onClick={()=>this.toggleColorPick()}>
-                            Rating
-                        </button>
-                        {this.showColorPick()}
+                    <div id="margin">Notes Name: <input id="title" type="text" name="title"
+                                                        onChange={(e) => this.saveNotesName(e)} defaultValue={this.state.notesName}/></div>
+                    <textarea onChange={(e) => this.saveNote(e)} defaultValue={this.state.note} placeholder="Write your note here!" id="text" name="text" rows="4"
+                              >{}</textarea>
+                </form>
+                <button id="button"  className={this.state.colorPick}  onClick={()=>this.toggleColorPick()}>
+                    Color
+                </button>
+                {this.showColorPick()}
 
-                </div>
-                <div>
-            <textarea rows="4" cols="50" className="Note" id="input2" placeholder="Write your note here!"
-                      onChange={(e) => this.saveNote(e)} defaultValue={this.state.note}>{}</textarea>
-                </div>
             </div>
         }
     }
 
+    showNoteDiv() {
+        if (this.state.showNoteD) {
+            return <div id="wrapper">
+                <form id="paper" method="get" action="">
+                <textarea onClick={() => this.openEditNote()} defaultValue={this.state.showNote} id="text" name="text" rows="4"
+                >{}</textarea>
+                </form>
+            </div>
+        } else {
+            return <div>{}</div>
+        }
+    }
     render() {
         var showDiv = this.showEditNote();
         return (
             <div className="App ">
                 <div className="Icons">
-                    <button className="tooltip2" id="Icon3" onClick={() => (this.setState({buttonNewNote: true})/this.newNote())}><FontAwesomeIcon
+                    <button className="tooltip2" id="Icon3" onClick={() => (this.setState({buttonNewNote: true,showEdit: false})/this.newNote())}><FontAwesomeIcon
                         icon="sticky-note"/><span className="tooltiptext2">New Note</span>
                     </button>
                     <button type="submit" className="tooltip1" id="Icon1" disabled={!this.state.isEnable}
                             onClick={() => this.showSavedNotesAndName()} ><FontAwesomeIcon
                         icon="plus"/><span className="tooltiptext1">Save Note</span>
                     </button>
-                    <button className="tooltip btn btn-warning" onClick={() => this.deleteNote()} id="Icon2"><FontAwesomeIcon
+                    <button className="tooltip btn btn-warning" disabled={this.state.isHidden}
+                            onClick={() => this.setState({showEdit: false, showNoteD: false, notesName: "", note: "" })/this.deleteNote()} id="Icon2"><FontAwesomeIcon
                         icon="trash"/>
                         <span className="tooltiptext">Delete Note</span>
                     </button>
                 </div>
                 <ul className="WrittenNotes">
                     {this.writtenNotesName.map((item, i) => {
-                        return <div className={this.color[i]} key={i + 1}>
-                            <button key={i} onClick={() => this.ifHidden(i)}>
+                        return <div className={this.color[i]} id="allNotes" key={i + 1}>
+                            <div id="justNotes" key={i} onClick={() => this.ifHidden(i)}>
                                 {item}
-                            </button>
-                            <div key={i + 2}>{this.time[i]}</div>
+                            </div>
+                            <div id="justTime" key={i + 2}>{this.time[i]}</div>
                         </div>
                     })} </ul>
 
-                <div className="nameAndNote">
-                    {this.newNote()}
-                </div>
-                <div onClick={() => this.openEditNote()}>{this.state.showNote}</div>
-                {showDiv}
+                <div className="nameAndNote">{this.newNote()}{this.showNoteDiv()}{showDiv}</div>
             </div>
         );
     }
